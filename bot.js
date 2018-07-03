181cf7e118aa51dd376f51f3d2dcb49feb8ebfb3
abotfora <40803952+abotfora@users.noreply.github.com>
2018-07-03 19:42:30 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "-"
+
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Client Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+
+
+client.on('ready', () => {
+     client.user.setActivity("-help",{type: 'WATCHING'})
+
+});
+clinet.login(process.env.BOT.TOKEN)
